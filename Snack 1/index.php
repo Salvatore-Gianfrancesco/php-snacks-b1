e3b0ee40bb62825d302aa849e713cff78837803e
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-28 12:27:34 +0100

    Snack 1 (added keys name)

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,13 +1,39 @@
 <?php
 
 $games = [
-    ['Milano', 'Brescia', 65, 41],
-    ['Roma', 'Caserta', 78, 87],
-    ['Napoli', 'Bolzano', 35, 58],
-    ['Palermo', 'Ascoli', 85, 55],
-    ['Firenze', 'Genova', 99, 75]
+    [
+        'home_team' => 'Milano',
+        'away_team' => 'Brescia',
+        'home_points' => 65,
+        'away_points' => 41
+    ],
+    [
+        'home_team' => 'Roma',
+        'away_team' => 'Caserta',
+        'home_points' => 78,
+        'away_points' => 87
+    ],
+    [
+        'home_team' => 'Napoli',
+        'away_team' => 'Bolzano',
+        'home_points' => 58,
+        'away_points' =>   35,
+
+    ],
+    [
+        'home_team' => 'Palermo',
+        'away_team' => 'Ascoli',
+        'home_points' => 85,
+        'away_points' => 55
+    ],
+    [
+        'home_team' => 'Firenze',
+        'away_team' => 'Genova',
+        'home_points' => 99,
+        'away_points' => 75
+    ]
 ];
 
 foreach ($games as $game) {
-    echo "$game[0] - $game[1] | $game[2] - $game[3]" . '<br>';
+    echo "$game[home_team] - $game[away_team] | $game[home_points] - $game[away_points]" . '<br>';
 }
